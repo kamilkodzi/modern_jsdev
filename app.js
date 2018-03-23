b6c5bf48373f5e0e9cbc19630fdd25f82f6d0940
Kamil <bizkamiladamski@gmail.com>
2018-03-23 12:55:38 +0000

    trying to make modular site. i hope it will be helpful leater

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+ app.set("view engine","ejs");
+ 
+app.use(express.static("modules"));
+
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname+'/modules/js_poczatek/index.ejs'));
+});
+  
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("server is runing");
+});
---
+++ modules/js_poczatek/app.js
@@ -0,0 +1,22 @@
+//Primitives
+
+//Strings
+const name =true;
+
+let test
+
+//Symbols
+const sym = Symbol();
+
+console.log(sym);
+
+
+const hobbies = ["movies","music"];
+const adress ={
+    city: "Boston",
+    state: "Ma"
+}
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
