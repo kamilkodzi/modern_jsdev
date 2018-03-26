e6adbb377c3b51811b5fd2ce51d1f9ee93fb2fda
Kamil <bizkamiladamski@gmail.com>
2018-03-26 15:04:36 +0000

    nothing exciting in mathObject

---
+++ modules/mathObject/app.js
@@ -0,0 +1,28 @@
+const num1=100;
+const num2=50;
+let val;
+
+//Simple Maths with numbers
+
+val=num1+num2;
+val=num1*num2;
+val=num1%num2;
+
+//math object
+val=Math.PI;
+val=Math.E;
+
+val=Math.round(2.8);
+
+val=Math.ceil(2.4);
+val=Math.floor(2.8);
+val=Math.sqrt(64);
+val=Math.abs(-20);
+val=Math.pow(8,2);
+val=Math.min(2,33,4,1,45,58,14,-2);
+val=Math.max(2,33,4,1,45,58,14,-2);
+val=Math.random();
+
+val=Math.floor(Math.random()*20 + 1);
+
+console.log(val);
