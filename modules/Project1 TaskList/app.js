e2a851635edfec2270a6d7d2ae36fdeb7def508a
Kamil <bizkamiladamski@gmail.com>
2018-04-20 13:33:25 +0000

    we can add new task now

--- modules/Project1 TaskList/app.js
+++ modules/Project1 TaskList/app.js
@@ -29,7 +29,20 @@ function addTask(e){
     li.className="collection-item";
     //Create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link=document.createElement('a');
+    //Add class
+    link.className="delete-item secondary-content";
+    // Add icon html
+    link.innerHTML='<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
     
+    //Append li to ul
+    taskList.appendChild(li);
+    
+    //Clear input
+    taskInput.value='';
     
     
     e.preventDefault();
