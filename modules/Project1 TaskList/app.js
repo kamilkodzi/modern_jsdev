e61e2617c3317af7474d95fff8826379c28c7e4d
Kamil <bizkamiladamski@gmail.com>
2018-04-23 12:54:33 +0000

    local storage function

--- modules/Project1 TaskList/app.js
+++ modules/Project1 TaskList/app.js
@@ -43,9 +43,12 @@ function addTask(e){
     link.innerHTML='<i class="fa fa-remove"></i>';
     //Append the link to li
     li.appendChild(link);
-    
     //Append li to ul
     taskList.appendChild(li);
+    //Store in LS
+    storeTaskLocalStorage(taskInput.value);
+    
+    
     
     //Clear input
     taskInput.value='';
@@ -53,6 +56,22 @@ function addTask(e){
     
     e.preventDefault();
 }
+
+
+// Store Task
+function storeTaskLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    
+    tasks.push(task);
+    localStorage.setItem('task',JSON.stringify(tasks));
+}
+
+
 
 //Remove Task
 function removeTask(e){
