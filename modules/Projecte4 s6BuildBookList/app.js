998c88154ba5f5448593ad6742afd70b2e55e80e
Kamil <bizkamiladamski@gmail.com>
2018-04-30 19:24:55 +0000

    added alerts with remove function after 3 saeconds, and upgrade event listener which create ui form class UI

--- modules/Projecte4 s6BuildBookList/app.js
+++ modules/Projecte4 s6BuildBookList/app.js
@@ -24,6 +24,26 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 };
 
+// Show alert
+UI.prototype.showAlert= function(message,className){
+    // Create div
+    const div = document.createElement('div');
+    //Add classes
+    div.className=`alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parrent 
+    const container= document.querySelector('.container');
+    // Get form
+    const form=document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div,form);
+    
+    // Timeout after 3 second
+    setTimeout(function(){
+       document.querySelector('.alert').remove(); 
+    },3000);
+};
 
 // Clear fields
 UI.prototype.clearFields = function(){
@@ -47,12 +67,22 @@ document.getElementById('book-form').addEventListener('submit',function(e){
     // Instantiate UI
     const ui = new UI();
     
-    // Add book to list
-    ui.addBookToList(book);
-    
-    // Clear fields
-    ui.clearFields();
-    
+    // Validate
+    if(title==='' || author === '' || isbn===''){
+        // Error alert
+        ui.showAlert('Please fill in all fields','error');
+        
+        
+    }else{
+        // Add book to list
+        ui.addBookToList(book);
+        
+        // Show succes
+        ui.showAlert('Book Added!','succes');
+        
+        // Clear fields
+        ui.clearFields(); 
+    }
     
     e.preventDefault();
 })
