1d012d3d2dda3fe2b0bf68e668c6142208ef456e
Kamil <bizkamiladamski@gmail.com>
2018-05-07 07:27:55 +0000

    full local strorage fixed

--- modules/Projecte4 s6BuildBookList/appES6.js
+++ modules/Projecte4 s6BuildBookList/appES6.js
@@ -67,21 +67,36 @@ class Store {
     }
     
     static displayBook(){
+        const books = Store.getBooks();
         
+        books.forEach(function(book){
+            const ui = new UI;
+            
+            // Add book to UI
+            ui.addBookToList(book);
+        });
     }
     
     static addBook(book){
         const books = Store.getBooks();
-        
         books.push(book);
-        
         localStorage.setItem('books',JSON.stringify(books));
     }
     
-    static romoveBook(){
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book,index){
+            if(book.isbn === isbn){
+                books.splice(index,1);
+            }
+        });
         
+        localStorage.setItem('books',JSON.stringify(books));
     }
 }
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded',Store.displayBook);
 
 
 // Event Listeners for add book
@@ -123,7 +138,12 @@ document.getElementById('book-form').addEventListener('submit',function(e){
 document.getElementById('book-list').addEventListener('click',function(e){
     // Instantiate UI
     const ui = new UI();
+    
+    // Delete book
     ui.deleteBook(e.target);
+    
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
     
     // Show message
     ui.showAlert('Book Removed!','succes');
