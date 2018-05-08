bb2d11e31a8e7365b927eb704ce79d94eac920c1
Kamil <bizkamiladamski@gmail.com>
2018-05-08 12:32:44 +0000

    API and callbacks Ajax style

---
+++ modules/ajaxSandbox/app.js
@@ -0,0 +1,48 @@
+const posts = [
+        {title:"Post One", body:"This i post one"},
+        {title:"Post One", body:"This i post one"}
+    ];
+    
+// function createPost(post){
+//     setTimeout(function(){
+//         posts.push(post);
+//     },2000);
+// }
+
+// function getPosts(){
+//     setTimeout(function(){
+//         let output='';
+//         posts.forEach(function(post){
+//             output+=`<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML=output;
+//     },1000);
+// }
+
+
+// //Synchronous way 
+// createPost({title:"Post three",body:"This is post three"});
+
+// getPosts();
+
+
+//Asynchronous way
+function createPost(post,callback){
+    setTimeout(function(){
+        posts.push(post);
+        callback();
+    },2000);
+}
+
+function getPosts(){
+    setTimeout(function(){
+        let output='';
+        posts.forEach(function(post){
+            output+=`<li>${post.title}</li>`;
+        });
+        document.body.innerHTML=output;
+    },1000);
+}
+
+
+createPost({title:"Post three",body:"This is post three"},getPosts);
