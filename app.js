e7af7c3911737b550b3f6ca6e8b773cda7950a0e
Kamil <bizkamiladamski@gmail.com>
2018-05-08 08:56:53 +0000

    how to asynchronous JSON data + nice design pattern

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("modules"));
 
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname+'/modules/ajaxXHRMethods/index.ejs'));
+  res.render(path.join(__dirname+'/modules/ajaxAndJSON/index.ejs'));
 });
   
 
