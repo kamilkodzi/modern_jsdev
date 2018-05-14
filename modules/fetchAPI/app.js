e9a618caf01f248b0832d7020f604d4f81c8b84a
Kamil <bizkamiladamski@gmail.com>
2018-05-14 12:31:25 +0000

    fetch API with local and external source

---
+++ modules/fetchAPI/app.js
@@ -0,0 +1,56 @@
+document.getElementById('button1').addEventListener('click',getText);
+document.getElementById('button2').addEventListener('click',getJson);
+document.getElementById('button3').addEventListener('click',getExternal);
+
+//Get Local text file
+function getText(){
+    fetch('fetchAPI/test.txt')
+        .then(function(res){
+            return (res.text());
+    })
+    .then(function(data){ // there should be arrow function- it will be soon
+       console.log(data); 
+       document.getElementById('output').innerHTML=data;
+    })
+    .catch(function(err){
+       console.log(err); 
+    });
+}
+
+//Get Local json file
+function getJson(){
+    fetch('fetchAPI/post.json')
+        .then(function(res){
+            return res.json();
+    })
+    .then(function(data){ // there should be arrow function- it will be soon
+       console.log(data);
+       let output = '';
+       data.forEach(function(post){
+          output+=`<li>${post.title}</li>`;
+       });
+       document.getElementById('output').innerHTML=output;
+    })
+    .catch(function(err){
+       console.log(err); 
+    });
+}
+
+//Get External API
+function getExternal(){
+    fetch('https://api.github.com/users')
+        .then(function(res){
+            return res.json();
+    })
+    .then(function(data){ // there should be arrow function- it will be soon
+       console.log(data);
+       let output = '';
+       data.forEach(function(user){
+          output+=`<li>${user.login}</li>`;
+       });
+       document.getElementById('output').innerHTML=output;
+    })
+    .catch(function(err){
+       console.log(err); 
+    });
+}
