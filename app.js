f6d005f3edd86dc446b0130c665939ecbd5ea464
Kamil <bizkamiladamski@gmail.com>
2018-05-16 11:50:08 +0000

    change in to async functions with fetch

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("modules"));
 
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname+'/modules/asyncAwait/index.ejs'));
+  res.render(path.join(__dirname+'/modules/easyHttpv3/index.ejs'));
 });
   
 
---
+++ modules/easyHttpv3/easyhttp.js
@@ -0,0 +1,58 @@
+/**
+ * Easy HTTP Library
+ * Library for making HTTP requests
+ * 
+ * @version 3.0.0
+ * @author kamilkodzi
+ * @licence MIT
+ * 
+ * */
+ 
+ class EasyHTTP{
+     // Make an HTTP GET Request
+     async get(url){
+     const response = await fetch(url);
+     const resData = await response.json();
+     return resData;
+    }
+    
+    // Make an HTTP POST Request
+    async post(url,data){
+         const response = await fetch(url,{
+             method:'POST',
+             headers:{
+             'Content-type':'application/json'
+             },
+             body: JSON.stringify(data)
+         });
+         const resData = await response.json();
+         return resData;
+    }
+    
+    
+    // Make an HTTP POST Request
+    async put(url,data){
+    const response= await fetch(url,{
+             method:'PUT',
+             headers:{
+             'Content-type':'application/json'
+             },
+             body: JSON.stringify(data)
+         });
+         const resData = await response.json();
+         return resData;
+    }
+    // Make an HTTP Delete Request
+     async delete(url){
+         const response= await fetch(url,{
+             method:'DELETE',
+             headers:{
+             'Content-type':'application/json'
+             }
+         });
+         const resData= await 'Resource Deleted...';
+         return resData;
+    }
+    
+    
+ }
