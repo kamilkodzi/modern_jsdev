739c123a5e71d8e6435d3d5efcf5be13b7f9b323
Kamil <bizkamiladamski@gmail.com>
2018-05-22 15:36:42 +0000

    regular expressions another part of information

--- modules/regularExpression2/app.js
+++ modules/regularExpression2/app.js
@@ -1,34 +1,51 @@
 let re;
-re=/hello/;
-re=/hello/g; // i = case insensitive
-// re=/hello/g; // Global search
+// Literal Characters
+re= /hello/;
+re= /hello/i;
 
-// console.log(re);
-// console.log(re.source);
+// Metacharacter Sybols
+re=/^h/i;       // Must start with and case insensitive
+re=/World$/i;   // Must start with and case insensitive
+re=/^hello$/i;  // Must begin and end with
+re=/^h.llo$/i;  // . Matches any ONE character
+re=/h*llo/i;  // * Matches any character 0 or more times
+re=/gre?a?y/i;  // ? optional character
+re=/Gre?a?y\?/;  //  Escape character
 
-// exec()- Return result in an array or null
-// const result= re.exec('brad hello world');
+// Brackets [] - Character Sets
+re=/gr[ae]y/i;  // Must be an a or e
+re=/[GF]ray/i;  // Must be an G or F 
+re=/[^GF]ray/i;  // Match anything except G or F if we want that should start with F or G we have to move it before brackets-> ^[GF]
+re=/[A-Z]ray/; // Match any uppercase letter
+re=/[a-z]ray/; // Match any lowercase letter
+re=/[A-Za-z]ray/; // Match any letter
+re=/[0-9][0-9]ray/;  // Match any digit
 
-// console.log(result);
-// console.log(result[0]);
-// console.log(result.index);
-// console.log(result.input);
 
-// test() - Return true or fals 
-// const result = re.test('Hello');
-// console.log(result);
+// Braces {} - Quantifiers
+re=/Hel{2,4}o/i;  // Must occur exacly {m} amount of times
+re=/Hel{2,}o/i;  // Must occur at least {m} times
 
-// match() - Return result array or null
-// const str= 'Hello There';
-// const result= str.match(re);
-// console.log(result);
+// Parentheses () - Grouping
+re=/^([0-9]x){3}$/;
 
-// search() - Return index od the first match if not found returns -1
-// const str="hello There hello";
-// const result=str.search(re);
-// console.log(result);
 
-// replace() - Return new string with some or all matches of a pattern
-const str='hello There hello';
-const newStr =str.replace(re,'hi');
-console.log(newStr);
+
+// String to match
+const str='3x3x3x3x3x3x3x3x3x3x3x3x';
+
+
+// Log Results
+const result=re.exec(str);
+console.log(result);
+
+
+function reTest(re,str){
+    if(re.test(str)){
+        console.log(`${str} matched ${re.source}`);
+    }else{
+        console.log(`${str} does NOT matched ${re.source}`);
+    }
+}
+
+reTest(re,str);
