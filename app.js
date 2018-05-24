00f3dcd2de8d0b915024fe707671c62745b8e3e4
Kamil <bizkamiladamski@gmail.com>
2018-05-24 13:52:46 +0000

    how to use iterator

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("modules"));
 
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname+'/modules/symbols/index.ejs'));
+  res.render(path.join(__dirname+'/modules/Project9 ProfilScroller/index.ejs'));
 });
   
 
