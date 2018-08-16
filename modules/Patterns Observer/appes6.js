a0c6e77a10370a687bad2549d1dea5efd9b19080
Kamil <bizkamiladamski@gmail.com>
2018-08-16 19:14:34 +0000

    Observer pattern es5 and es6 looks

---
+++ modules/Patterns Observer/appes6.js
@@ -0,0 +1,55 @@
+class EventObserver{
+    constructor(){
+        this.observers=[];
+    }
+    subscribe(fn){
+        this.observers.push(fn);
+        console.log(`you are now subscribed to ${fn.name}`);
+    }
+    
+    unsubscribe(fn){
+        // Filter out from list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of ovservers.
+        this.observers=this.observers.filter(function(item){
+            if(item !== fn){ 
+                return item;
+            }
+        });
+        console.log(`you are now unsubscribed from ${fn.name}`);
+    }
+    
+    fire(){
+        this.observers.forEach(function(item){
+        item.call();
+    })}
+}
+
+const click= new EventObserver ();
+
+
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click',function(){
+    click.subscribe(getCurMilliseconds);
+});
+document.querySelector('.unsub-ms').addEventListener('click',function(){
+    click.unsubscribe(getCurMilliseconds);
+});
+document.querySelector('.sub-s').addEventListener('click',function(){
+    click.subscribe(getCurSeconds);
+});
+document.querySelector('.unsub-s').addEventListener('click',function(){
+    click.unsubscribe(getCurSeconds);
+});
+document.querySelector('.fire').addEventListener('click',function(){
+    click.fire();
+});
+
+
+// Click Handler
+const getCurMilliseconds=function(){
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
+const getCurSeconds=function(){
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+};
+
+
